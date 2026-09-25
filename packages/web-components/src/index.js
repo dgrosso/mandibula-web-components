a7b0372da4063f413e22910a4ed0505da4d5ab4f
Daniel Grosso <daniel@onadev.ar>
2026-09-24 23:28:27 -0300

    Export slider from web-components bundle

--- packages/web-components/src/index.js
+++ packages/web-components/src/index.js
@@ -3,6 +3,7 @@ import "@mandibula/media-slot";
 import "@mandibula/pointer";
 import "@mandibula/responsive-text";
 import "@mandibula/scoped-inline-svg";
+import "@mandibula/slider";
 import "@mandibula/spinner";
 import "@mandibula/suspense";
 import "@mandibula/video";
